b6b870fd4fd1ddb2abf9024d8bdd39250e5773d5
shinta0806 <shinta.0806@gmail.com>
2025-03-19 11:35:15 +0900

    未使用の ViewModel を削除。

--- TestWindowSubclass/Views/MainWindows/MainPage.xaml.cs
+++ TestWindowSubclass/Views/MainWindows/MainPage.xaml.cs
@@ -1,19 +1,11 @@
 using Microsoft.UI.Xaml.Controls;
 
-using TestWindowSubclass.ViewModels.MainWindows;
-
 namespace TestWindowSubclass.Views.MainWindows;
 
 public sealed partial class MainPage : Page
 {
-	public MainViewModel ViewModel
-	{
-		get;
-	}
-
 	public MainPage()
 	{
-		ViewModel = new();
 		InitializeComponent();
 	}
 }
